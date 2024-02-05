6b6c0a02ee408c4e3d7a8599dcb243d2885061f2
tbm98 <tbm98.dev@gmail.com>
2024-02-06 01:24:14 +0700

    xx

--- lib/flutter_good_ads.dart
+++ lib/flutter_good_ads.dart
@@ -6,5 +6,6 @@ export 'src/banner/adaptive/good_banner_adaptive_anchored.dart';
 export 'src/banner/adaptive/good_banner_adaptive_inline.dart';
 export 'src/banner/standard/good_banner_standard.dart';
 export 'src/good_ads.dart';
+export 'src/good_app_open.dart';
 export 'src/good_interstitial.dart';
 export 'src/good_rewarded.dart';
